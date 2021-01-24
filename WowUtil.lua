f798ab6765bce64ebd20c56b02aea0d7495ee530
Logan Payton <Logan.Payton@javs.com>
2021-01-24 11:03:47 -0600

    Raid Target

--- WowUtil.lua
+++ WowUtil.lua
@@ -1,7 +1,15 @@
-local AddOnName, WowUtil = ...
+local _, AddOn = ...
+WowUtil = AddOn
 
-function WowUtil_Debug()
-    print("AddOnName: " .. AddOnName)
-    print("WowUtil: ")
-    print(WowUtil)
+local UnitName = UnitName
+local SetRaidTarget = SetRaidTarget
+
+function AddOn:SetRaidTarget(icon)
+    local target = "player"
+    if UnitName("mouseover") ~= nil then
+        target = "mouseover"
+    elseif UnitName("target") ~= nil then
+        target = "target"
+    end
+    SetRaidTarget(target, icon)
 end
